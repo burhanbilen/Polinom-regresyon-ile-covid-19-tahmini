52e07f0ae081755bc126def2e3fd5431850402eb
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2020-10-08 14:20:29 +0300

    Update poly_reg.py

--- poly_reg.py
+++ poly_reg.py
@@ -21,14 +21,10 @@ gunluk_iyilesen_sayisi = imp.fit_transform(gunluk_iyilesen_sayisi)
 pf = PolynomialFeatures(degree = 11) 
 X_Polynom = pf.fit_transform(X)
 polynom = LinearRegression()
-lr = LinearRegression()
 
 
 polynom.fit(X_Polynom, gunluk_hasta_sayisi)
 p = polynom.predict(X_Polynom)
-lr.fit(X, gunluk_hasta_sayisi)
-plt.plot(X, lr.predict(X), color="limegreen")
-
 plt.scatter(X, gunluk_hasta_sayisi, marker = '.', s = 15, color = "grey")
 plt.plot(X, p, color = "black")
 plt.title('Günlere Göre Hastalık Sayısı')
@@ -39,9 +35,6 @@ plt.show()
 
 polynom.fit(X_Polynom, gunluk_test_sayisi)
 p = polynom.predict(X_Polynom)
-lr.fit(X, gunluk_test_sayisi)
-plt.plot(X, lr.predict(X), color="limegreen")
-
 plt.scatter(X, gunluk_test_sayisi, marker='o', s=15, color = "grey")
 plt.plot(X, p,  color="black") 
 plt.title('Günlere Göre Test Sayısı')
@@ -52,9 +45,6 @@ plt.show()
 
 polynom.fit(X_Polynom, gunluk_vefat_sayisi)
 p = polynom.predict(X_Polynom)
-lr.fit(X, gunluk_vefat_sayisi)
-plt.plot(X, lr.predict(X), color="limegreen") 
-
 plt.scatter(X, gunluk_vefat_sayisi, marker='o', s=15, color = "grey")
 plt.plot(X, p,  color="black") 
 plt.title('Günlere Göre Vefat Sayısı')
@@ -65,9 +55,6 @@ plt.show()
 
 polynom.fit(X_Polynom, gunluk_iyilesen_sayisi)
 p = polynom.predict(X_Polynom)
-lr.fit(X, gunluk_iyilesen_sayisi)
-plt.plot(X, lr.predict(X), color="limegreen") 
-
 plt.scatter(X, gunluk_iyilesen_sayisi, marker='o', s=15, color = "grey")
 plt.plot(X, p,  color="black") 
 plt.title('Günlere Göre İyileşen Sayısı')
